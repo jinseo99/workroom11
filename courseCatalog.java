b549aca04204a02717a518b87d11fc8aca12fc42
PotatoEngineer <43867559+jinseo99@users.noreply.github.com>
2020-10-17 13:02:42 -0600

    asdf

--- courseCatalog.java
+++ courseCatalog.java
@@ -1,8 +1,10 @@
-import org.json.simple.JSONObject;
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
 
 public class courseCatalog {
     
     public static void main(){
-       Objet  
+        JSONParser parser = new JSONParser();
+
     }
 }
