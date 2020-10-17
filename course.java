b8e569f2b3c4ad9e58214bbe97d19d7457ed2731
PotatoEngineer <43867559+jinseo99@users.noreply.github.com>
2020-10-17 11:52:11 -0600

    edit course

--- course.java
+++ course.java
@@ -1,3 +1,35 @@
 public class course {
-    
+    private String courseName;
+    private String title;
+    private lecture [] lectureList;
+    private tutorial [] tutorialList;
+
+    public course() {
+        courseName = null;
+        title = null;
+        lectureList = null;
+        tutorialList = null;
+    }
+    public String getCourseName(){
+        return courseName;
+    }
+    public void setCourseName(String name){
+        courseName = name;
+    }
+
+    public void availableLecture(){
+        for (int i = 0; i<lectureList.length; i++){
+            //lectureList[i].isAvail();
+        }
+    }
+
+    public void viewAllLectures(){
+
+    }
+    public void availableTutorial(){
+        for (int i = 0; i<tutorialList.length; i++){
+            //tutorialList[i].isAvail();
+        }
+    }
+
 }
