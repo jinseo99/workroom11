2e99dc5ea2f3109aeba36037f595c7cc5a9b420a
kameshwarasekar <kameshwarasekar@gmail.com>
2020-10-17 12:08:35 -0600

    Update tutorial.java

--- tutorial.java
+++ tutorial.java
@@ -1,3 +1,19 @@
-public class tutorial {
-    
+public class Tutorial {
+
+	
+	private int tutorialNum;
+	private int capacity;
+	private int seatsFilled;
+	private String instructor;
+	private Occurance[] occuranceList;
+	
+	public Tutorial(int tutorialNum, int capacity, int seatsFilled,
+			String instructor, Occurance[] occuraseList)
+	{
+	   this.tutorialNum=tutorialNum;
+	   this.capacity=capacity;
+	   this.instructor=instructor;
+	   this.occuranceList=occuranceList;
+	   this.seatsFilled=seatsFilled;
+	}
 }
