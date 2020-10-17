60e32a387720a159f3cf0fd2b490e3aeb9e3c167
Khaled Elmalawany <elmalawanykhaled@gmail.com>
2020-10-17 15:56:43 -0600

    Updated GUI with JComboBox

--- gui.java
+++ gui.java
@@ -1,18 +1,29 @@
+import java.awt.*;
 import javax.swing.*;
 
 public class gui{
     public static void main(String args[]){
-       JFrame frame = new JFrame("Courses Scheduler");
+        // Frame Details
+        JFrame frame = new JFrame("Courses Scheduler");
        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        frame.setSize(800,800);
 
-       JButton button1 = new JButton("Select Course");
-       JButton button2 = new JButton("Select Lecture");
-       JButton button3 = new JButton("Select Tutorial");
+       // Define the various options used
+       String coursesOptions[] = {"Math", "Science", "Biology"};
+       String lectureOptions[] = {"Lecture 1", "Lecture 2"};
+       String tutorialOptions[] = {"Tutorial 1", "Tutorial 2"};
 
-       frame.getContentPane().add(button1); // Adds Courses selection to content pane of frame
-       frame.getContentPane().add(button2); // Adds Lecture selection to content pane of frame
-       frame.getContentPane().add(button3); // Adds Tutorial selection to content pane of frame
+       // Create Combo Box instances of the selections
+       JComboBox courses = new JComboBox(coursesOptions);
+       JComboBox lectures = new JComboBox(lectureOptions);
+       JComboBox tutorials = new JComboBox(tutorialOptions);
+
+       // Add the 
+       frame.getContentPane().add(courses); // Adds Courses selection to content pane of frame
+       frame.getContentPane().add(lectures); // Adds Lecture selection to content pane of frame
+       frame.getContentPane().add(tutorials); // Adds Tutorial selection to content pane of frame
+
+       frame.setLayout(new FlowLayout(FlowLayout.LEFT));
        frame.setVisible(true);
     }
 }
--- student.java
+++ student.java
@@ -3,16 +3,16 @@ import java.util.*;
 public class Student {
     Vector <Course>courseList;
 
-    public student(){
-        courseList = new Vector<Course>();
-    }
-    public void addCourse(Course c){
-        courseList.append(c);
-    }
-    public void availableCourse (Course c){
-        c.availableCourse();
-    }
-    public void viewCourse(Course c) {
-        c.viewAllCourses();;
-    }
+    // public student(){
+    //     courseList = new Vector<Course>();
+    // }
+    // public void addCourse(Course c){
+    //     courseList.append(c);
+    // }
+    // public void availableCourse (Course c){
+    //     c.availableCourse();
+    // }
+    // public void viewCourse(Course c) {
+    //     c.viewAllCourses();;
+    // }
 }
