570596c36e4cf506846070ec7fce7f52407a54ff
kameshwarasekar <kameshwarasekar@gmail.com>
2020-10-17 16:27:14 -0600

    Enhancements

---
+++ department.java
@@ -0,0 +1,4 @@
+public class department {
+    private course [] courseList;
+
+}
--- student.java
+++ student.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class Student {
     Vector <Course>courseList;
+<<<<<<< Updated upstream
 
     // public student(){
     //     courseList = new Vector<Course>();
@@ -15,4 +16,20 @@ public class Student {
     // public void viewCourse(Course c) {
     //     c.viewAllCourses();;
     // }
+=======
+    String name;
+    public student(String name){
+        courseList = new Vector<Course>();
+        this.name=name;
+    }
+    public void addCourse(Course c){
+        courseList.append(c);
+    }
+    public void availableCourse (Course c){
+        c.availableCourse();
+    }
+    public void viewCourse(Course c) {
+        c.viewAllCourses();;
+    }
+>>>>>>> Stashed changes
 }
