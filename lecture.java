7ed10c37c7da9eaa5f93e8893bff499dbfa9039a
kameshwarasekar <kameshwarasekar@gmail.com>
2020-10-17 12:07:19 -0600

    Update lecture.java

--- lecture.java
+++ lecture.java
@@ -1,3 +1,19 @@
-public class lecture {
-    
+public class Lecture {
+
+	private int lectureNum;
+	private int capacity;
+	private int seatsFilled;
+	private String instructor;
+	private Occurance[] occuranceList;
+	
+	public Lecture(int lectureNum, int capacity, int seatsFilled,
+			String instructor, Occurance[] occuraseList)
+	{
+	   this.lectureNum=lectureNum;
+	   this.capacity=capacity;
+	   this.instructor=instructor;
+	   this.occuranceList=occuranceList;
+	   this.seatsFilled=seatsFilled;
+	}
+	
 }
